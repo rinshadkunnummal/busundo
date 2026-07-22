f4c8df1dbb555640a2c1a5ecf047c962eeb95132
rinshadkl <riinshad18@gmail.com>
2026-07-22 15:49:36 +0530

    feat: add Inter and Sora fonts with Tailwind CSS configuration and entry point setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+// Local fonts
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/sora/400.css";
+import "@fontsource/sora/600.css";
+import "@fontsource/sora/700.css";
+
 import { router } from "./app/router";
 import "./index.css";
 
